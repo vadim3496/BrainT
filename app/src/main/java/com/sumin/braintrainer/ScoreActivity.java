83eedf7a903885fa9a5468924b7bbc37aaf5c6c9
vadim3496 <58651543+vadim3496@users.noreply.github.com>
2019-12-17 15:33:16 +0200

    commit v1.0

---
+++ app/src/main/java/com/sumin/braintrainer/ScoreActivity.java
@@ -0,0 +1,34 @@
+package com.sumin.braintrainer;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    private TextView textViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+        textViewResult = findViewById(R.id.textViewResult);
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("result")) {
+            int result = intent.getIntExtra("result", 0);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            int max = preferences.getInt("max", 0);
+            String score = String.format("Ваш результат: %s\nМаксимальный результат: %s", result, max);
+            textViewResult.setText(score);
+        }
+    }
+
+    public void onClickStartNewGame(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
